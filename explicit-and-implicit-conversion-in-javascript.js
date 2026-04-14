2f73eb7af514d91698755b4c88ca33ed6176bf5b
DethPlague98 <tnaes98@gmail.com>
2026-04-13 21:49:22 -0500

    after edits

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -20,13 +20,38 @@ Use console.log() to clearly show the before-and-after type conversions.
 
 
 let result = "5" - 2;
-console.log("The result is: " + result);
+console.log("The result is: " + result); // this section runs fine it seems
 
-let isValid = Boolean("false");
+let isValid = Boolean("false"); //this is true because it is a boolean with string
+let correct = null;
+isValid = Number(correct); //isValid now is null and false
+
 if (isValid) {
-    console.log("This is valid!");
+    console.log("This is valid!"); //This should not run
 }
 
 let age = "25";
 let totalAge = age + 5;
-console.log("Total Age: " + totalAge);
+//console.log("Total Age: " + totalAge); //prints 255 not 30
+let newAge = Number(age);
+console.log("Total Age: " + newAge); //This works as age is not a string when this line is executed
+
+
+////////////////Task number 2//////////////////////////
+let number = 63;
+console.log(typeof number);
+
+let sentence = "I can count at least... " + number + " bananas here.";
+console.log(sentence) //implicit type conversion
+console.log(typeof sentence); //Number to string for "number" 
+
+let secondTask = 0;
+if (secondTask) {
+  console.log("This worked!"); //explicit type conversion, this gets skipped due to false.
+}
+
+secondTask = String(secondTask);
+
+if (secondTask) {
+  console.log("This worked, as intended!"); //explicit type conversion, prints after returns true.
+}
